dadd47eda3d02b3b29e24f522bc56db47e47aba4
rowsen2904 <bayramovw0@gmail.com>
2025-07-24 12:55:31 +0500

    created ActivateInviteCodeView view class

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from .views import ActivateInviteCodeView
+
 urlpatterns = [
+    path('profile/activate-invite/',
+         ActivateInviteCodeView.as_view(), name='activate-invite'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.utils.translation import gettext_lazy as _
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .serializers import ActivateInviteCodeSerializer
+
+
+class ActivateInviteCodeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = ActivateInviteCodeSerializer(
+            data=request.data, context={"request": request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": _("Invite code successfully activated")})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
